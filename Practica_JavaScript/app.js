bf298306659e3928ca23de6167c3f41f661ca279
Raque25 <raquelvelascofranco@gmail.com>
2020-04-24 19:21:00 -0500

    5. Tipo especial "undefined"

--- Practica_JavaScript/app.js
+++ Practica_JavaScript/app.js
@@ -1,4 +1,4 @@
-var a = 1;
+/*var a = 1;
 var b ="Raquel";
 
 console.log(a);
@@ -6,4 +6,16 @@ console.warn(a);
 console.error(a);
 console.info(a);
 
-console.log(a+a);
+console.log(a+a);*/
+
+
+//mandará undefined si se ponen primero la ejecución de variables y después su declaración
+//undefined != (null && error) ya que se pueden utilizar aun en consola
+console.log(a);
+console.log(b);
+console.log(c);
+
+var a = "Raquel";
+var b = 3;
+var c = true;
+
