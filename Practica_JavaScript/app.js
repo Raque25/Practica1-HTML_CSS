73699fea04910f172f9a7af0ca12361d187eba9a
Raque25 <raquelvelascofranco@gmail.com>
2020-04-24 19:40:49 -0500

    6. Escritura dinámica

--- Practica_JavaScript/app.js
+++ Practica_JavaScript/app.js
@@ -11,11 +11,37 @@ console.log(a+a);*/
 
 //mandará undefined si se ponen primero la ejecución de variables y después su declaración
 //undefined != (null && error) ya que se pueden utilizar aun en consola
-console.log(a);
+/*console.log(a);
 console.log(b);
 console.log(c);
 
 var a = "Raquel";
 var b = 3;
-var c = true;
+var c = true;*/
 
+var num = 10; //primitivo
+var str = "texto"; //primitivo
+var bol = true; //primitivo
+var und = undefined; //primitivo
+var nulo = null; //primitivo
+
+//escritura dinámica (las variables no estan amarradas a un tipo en particular)
+num = str;
+console.log(num);
+
+bol = 10;
+console.log(bol);
+
+//objetos
+var obj = {
+    //nomenclatura de pares
+    numero: 10, //propiedad
+    texto: "otro texto", //si es una nomenclatura no cierra con punto y coma, si tiene más elementos, poner coma, último elemento no lleva nada
+
+    objHijo: {
+        numero2: 20,
+        texto2: "otro texto 2"
+    }
+};
+
+console.log(obj);
